4b7e31a3c90875df77cfb5204480ea54c61a030a
RubenVanBreda <re.vanbreda@gmail.com>
2020-04-06 21:22:41 +0200

    First Commit

---
+++ game_init.c
@@ -0,0 +1,107 @@
+//
+// Created by Lili on 26/03/2020.
+//
+
+//
+// Created by Lili on 24/03/2020.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "game_init.h"
+
+#define MAX_CHAR 20
+
+void initialize_players(player players[PLAYERS_NUM]){
+
+    // implement here the functionality to initialize the players
+    char name_input[MAX_CHAR];
+    printf("%s\n",players[0].name);
+    //: Loop through the players
+    for (int i = 0; i < PLAYERS_NUM;  i++) {
+
+        //TODO: Get players name input from user
+
+        // Create a string for the players name
+        printf("Please enter player %d' name: ",i);
+        scanf("%s",&name_input);
+
+        // Initialise the values of player
+        players[i].name = name_input;
+        // Set the color of the player
+        players[i].player_color = i;
+        printf("%s\n",players[i].name);
+
+
+
+    }
+    printf("%s\n",players[0].name);
+
+}
+
+//Set Invalid Squares (where it is not possible to place stacks)
+set_invalid(square * s){
+s->type = INVALID;
+s->stack = NULL;
+s->num_pieces = 0;
+}
+
+//Set Empty Squares (with no pieces/stacks)
+set_empty(square * s){
+s->type = VALID;
+s->stack = NULL;
+s->num_pieces = 0;
+}
+
+//Set squares  with a GREEN piece
+set_green(square * s){
+s->type = VALID;
+s->stack = (piece *) malloc (sizeof(piece));
+s->stack->p_color = GREEN;
+s->stack->next = NULL;
+s->num_pieces = 1;
+}
+
+//Set squares with a RED piece
+set_red(square * s){
+s->type = VALID;
+s->stack = (piece *) malloc (sizeof(piece));
+s->stack->p_color = RED;
+s->stack->next = NULL;
+s->num_pieces = 1;
+}
+
+//initializes the board
+void initialize_board(square board [BOARD_SIZE][BOARD_SIZE]){
+
+    for(int i=0; i< BOARD_SIZE; i++){
+        for(int j=0; j< BOARD_SIZE; j++){
+            //invalid squares
+            if((i==0 && (j==0 || j==1 || j==6 || j==7)) ||
+               (i==1 && (j==0 || j==7)) ||
+               (i==6 && (j==0 || j==7)) ||
+               (i==7 && (j==0 || j==1 || j==6 || j==7)))
+                set_invalid(&board[i][j]);
+
+            else{
+                //squares with no pieces
+                if(i==0 || i ==7 || j==0 || j == 7)
+                    set_empty(&board[i][j]);
+                else{
+                    //squares with red pieces
+                    if((i%2 == 1 && (j < 3 || j> 4)) ||
+                       (i%2 == 0 && (j == 3 || j==4)))
+                        set_red(&board[i][j]);
+                        //green squares
+                    else set_green(&board[i][j]);
+                }
+            }
+        }
+
+
+    }
+
+
+}
+
---
+++ main.c
@@ -0,0 +1,34 @@
+//
+// Created by Lili on 26/03/2020.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "input_output.h"
+#include <fcntl.h>
+#include <zconf.h>
+#include <wchar.h>
+
+int main() {
+
+    // declaration of the players and the board
+    player players[PLAYERS_NUM];
+    square board[BOARD_SIZE][BOARD_SIZE];
+    initialize_players(players);
+
+
+
+    printf("%s\n",players[0].name);
+    initialize_board(board);
+    int turn = 0;
+    //START GAME
+
+    print_board(board);
+    return 0;
+}
+//
+//void GUI(){
+////    setmode(STDOUT_FILENO);
+//    wprintf(L"\x043a");
+//}
